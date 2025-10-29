5eed4f8e7718459088faf7c2be30cbe6b082bb9c
SWAY <www.florencesway@gmail.com>
2025-10-29 10:34:44 +0300

    api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 // API Configuration - Updated to match backend API guide
 export const API_CONFIG = {
 	// BASE_URL: 'https://mifumosms.servehttp.com/api',
-	BASE_URL: 'http://127.0.0.1:8000/api',
+	BASE_URL: 'https://mifumosms.servehttp.com/api',
 	ENDPOINTS: {
 		AUTH: {
 			LOGIN: '/auth/login/',
